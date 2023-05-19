6998eecaf06951f229f3fd7b49e7d0d0dd2f6050
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2023-05-19 10:12:52 +0900

    [Silver V] Title: 방 번호, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        num = new int[10];
+        for(char c: st.nextToken().toCharArray()){
+            int number = c-'0';
+            if(number == 9 || number == 6){
+                num[9] ++;
+            }else{
+                num[c-'0'] ++;
+            }
+
+        }
+
+        if (num[9] % 2 ==0){
+            num[9] /= 2;
+        }else{
+            num[9] = (num[9] / 2) + 1  ;
+
+        }
+
+        int result =0;
+        for(int i:num){
+            if(result < i){
+                result = i;
+            }
+        }
+
+        System.out.println(result);
+
+
+    }
+
+}
