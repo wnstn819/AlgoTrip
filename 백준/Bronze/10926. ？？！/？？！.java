f57beed1f854aca248bcc8e5bc7a0d83fc06d471
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2023-05-24 11:37:10 +0900

    [Bronze V] Title: ??!, Time: 140 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        public static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        System.out.println(str + "??!");
+
+        }
+
+
+    }
+
