e065f620ba9e4baec44b53f0a97a18fa96c1fd59
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2023-07-02 16:21:27 +0900

    [Bronze I] Title: 소인수분해, Time: 148 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        for(int i=2;i< n/2+1;i++){
+            while(n %i==0){
+                System.out.println(i);
+                n/=i;
+            }
+        }
+
+        if(n != 1){
+            System.out.println(n);
+        }
+
+
+
+
+    }
+
+}
