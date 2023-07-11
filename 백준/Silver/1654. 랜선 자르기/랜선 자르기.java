0de3c2a3a3c0e7d393aefc2f8d9ccba6c8db8dcb
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2023-07-11 21:53:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        public static int N,M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        long sum =0;
+        int[] arr = new int[N];
+        for (int i =0;i<N;i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+            if(sum<arr[i]){
+                sum = arr[i];
+            }
+        }
+
+        long start =0;
+        sum ++;
+        long mid = 0;
+        while(start<sum){
+
+            mid = (start +sum)/2;
+
+            long ans = 0;
+            for(int i: arr){
+                ans += (i/mid);
+            }
+
+            if(ans < M){
+               sum = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        System.out.println(start-1);
+
+
+
+
+
+    }
+
+}
