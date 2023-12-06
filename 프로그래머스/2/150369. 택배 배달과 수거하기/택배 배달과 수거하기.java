afb09ef3a9399c5481888ec3f59b1302d9deb389
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2023-12-06 17:35:08 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5.74 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,49 +1,18 @@
-import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        Deque<Integer> dque = new ArrayDeque<>();
-        Deque<Integer> pque = new ArrayDeque<>();
-        
-        for(int i = 0;i<n;i++){
-            dque.addFirst(deliveries[i]);
-            pque.addFirst(pickups[i]);
-        }    
+        int deliver = 0, pickup = 0;
+        for(int i = n-1; i >= 0; i--){
+            deliver += deliveries[i];
+            pickup += pickups[i];
+            while (deliver > 0 || pickup > 0){
+                deliver -= cap;
+                pickup -= cap;
+                answer += ((i+1) * 2);
+            }
 
-        while(!dque.isEmpty() || !pque.isEmpty() ){
-            while(!dque.isEmpty() && dque.peek() == 0){
-                dque.poll();
-            }
-            
-            while(!pque.isEmpty() && pque.peek() == 0){
-                pque.poll();
-            }
-            int dsize = dque.size();
-            int psize = pque.size();
-            int dcnt = cap;
-            int pcnt = cap;
-            while(dcnt > 0 && !dque.isEmpty()){
-                if(dque.peek() <= dcnt){
-                    dcnt -= dque.poll();
-                }else{
-                    dque.addFirst(dque.poll() - dcnt);
-                    dcnt = 0;
-                    
-                }
-            }
-            while(pcnt >0 && !pque.isEmpty()){
-                if(pque.peek() <= pcnt){
-                    pcnt -= pque.poll();
-                }else{
-                    pque.addFirst(pque.poll() - pcnt);
-                    pcnt = 0;
-                }
-            }
-            
-            answer += Math.max(dsize, psize) * 2;
-            
         }
-        
         return answer;
     }
 }
