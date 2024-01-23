a1497a9f1dd266a42a3c03b7ae5bb5b2ab23d9c1
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-01-23 22:58:15 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 49.08 ms, Memory: 218 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        int N = board.length;
+        int M = board[0].length;
+        int[][] list = new int[N+1][M+1];
+        int[][] dp = new int[N+1][M+1];
+        for(int i =0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                list[i][j] = board[i][j];
+            }
+        }
+        
+        for(int[] arr : skill){
+            int r1 = arr[1];
+            int c1 = arr[2];
+            int r2 = arr[3];
+            int c2 = arr[4];
+            int degree =arr[5];
+            if(arr[0] == 1){
+                dp[r1][c1] -= degree;
+                dp[r2+1][c1] += degree;
+                dp[r1][c2+1] += degree;
+                dp[r2+1][c2+1] -= degree;
+            }else{
+                dp[r1][c1] += degree;
+                dp[r2+1][c1] -= degree;
+                dp[r1][c2+1] -= degree;
+                dp[r2+1][c2+1] += degree;
+            }
+        }
+        
+        
+       for(int i =0;i<=M;i++){
+           for(int j=1;j<=N;j++){
+                dp[j][i] = dp[j-1][i] + dp[j][i]; 
+           }
+       }
+         
+        
+       for(int i =0;i<=N;i++){
+           for(int j=1;j<=M;j++){
+                dp[i][j] = dp[i][j-1] + dp[i][j]; 
+           }
+       }
+    
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                board[i][j] += dp[i][j];
+                if(board[i][j] >0){
+                    answer++;
+                }
+            }
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
