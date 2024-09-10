5caf893630380ad9fab1f08217033b5a5c36365c
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-09-10 21:10:25 +0900

    [level 2] Title: 조이스틱, Time: 14.10 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -7,8 +7,12 @@ class Solution {
         for(int i =0;i<name.length();i++){
             char c = name.charAt(i);  
             int a = ('Z' -'0' - 17) - (c - '0' - 17)+1;
+            System.out.println('Z'- '0' - 17);
             int b = (c - '0' - 17) - ('A' - '0' - 17);
-            int min = Math.min(Math.abs(a), Math.abs(b));
+            
+            System.out.println(" a : " + a +" b : " + b);
+            System.out.println((c - 'A') + " : " + ('Z' - c + 1));
+            int min = Math.min(c - 'A', 'Z' - c + 1);
             answer += min; 
             
         }
