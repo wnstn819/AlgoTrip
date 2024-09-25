ead14b1edbd9ccd119b59f43fbaf218a6b487ec7
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-09-25 14:29:05 +0900

    [Gold V] Title: 적록색약, Time: 144 ms, Memory: 16908 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,11 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-        public static int N,M,result =0,result2 =0;
-        public static int[][] list, list2;
-        public static boolean[][] visited;
-        public static int[] dx = {0,0,1,-1};
-        public static int[] dy = {1,-1,0,0};
+    public static int N, M, result1 = 0, result2=0;
+    public static int[][] map;
+    public static boolean[][] visited1, visited2;
+    public static int[] dx = {0,0,1,-1};
+    public static int[] dy = {1,-1,0,0};
+
 
 
     public static void main(String[] args) throws IOException {
@@ -14,78 +15,60 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+
+
         N = Integer.parseInt(st.nextToken());
-        list = new int[N][N];
-        list2 = new int[N][N];
-        visited = new boolean[N][N];
+        map = new int[N][N];
+        visited1 = new boolean[N][N];
+        visited2 = new boolean[N][N];
 
-        for(int i=0;i<N;i++){
-            String str = br.readLine();
-            for(int j=0;j<str.length();j++){
-                char a = str.charAt(j);
-                list[i][j] = a == 'R' ? 1 : a == 'G' ? 2 : 3;
-                list2[i][j] = a == 'B' ? 1 : 2;
-
+        for(int i =0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            for(int j=0;j<N;j++){
+                char color = str.charAt(j);
+                map[i][j] = color == 'R' ? 1 : color == 'G' ? 2 : 4 ;
             }
         }
-        for(int i=0;i<N;i++){
+
+        for(int i =0;i<N;i++){
             for(int j=0;j<N;j++){
-                if(!visited[i][j]){
-                    BFS(i,j,list);
-                    result++;
+                if(!visited1[i][j]){
+                    BFS(i,j,visited1,1);
+                    result1++;
                 }
-            }
-        }
-        visited = new boolean[N][N];
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(!visited[i][j]){
-                    BFS(i,j,list2);
+                if(!visited2[i][j]){
+                    BFS(i,j,visited2,2);
                     result2++;
                 }
             }
+
         }
 
-        System.out.println(result + " " + result2);
 
-
+        System.out.println(result1 + " " + result2);
     }
 
-    public static void BFS(int i , int j,int[][] type){
+    public static void BFS(int i, int j,boolean[][] visited, int type){
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[]{i,j});
         visited[i][j] = true;
-        Queue<Node> que = new LinkedList<>();
-        que.add(new Node(i,j));
-        int key = type[i][j];
 
-
-        while (!que.isEmpty()) {
-            Node node = que.poll();
+        while(!que.isEmpty()){
+            int[] arr = que.poll();
 
             for(int k=0;k<4;k++){
-                int nx = node.x + dx[k];
-                int ny = node.y + dy[k];
-                if(nx >= 0 && nx <N && ny >=0 && ny <N && !visited[nx][ny] ){
-                    if( key == type[nx][ny]){
-                        que.add(new Node(nx,ny));
-                        visited[nx][ny] = true;
-                    }
+                int nx = arr[0] + dx[k];
+                int ny = arr[1] + dy[k];
+                if(nx >=0 && nx < N && ny >=0 && ny < N && !visited[nx][ny] && (type ==2 ? Math.abs(map[i][j] - map[nx][ny]) < 2  :  map[i][j] == map[nx][ny])){
 
+                    que.add(new int[]{nx,ny});
+                    visited[nx][ny] = true;
                 }
-
             }
-
-
         }
 
-    }
 
-}
-class Node{
-    int x,y;
-    Node(int x,int y){
-        this.x=x;
-        this.y=y;
     }
-
 }
+
