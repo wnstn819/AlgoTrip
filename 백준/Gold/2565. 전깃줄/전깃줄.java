e9c9df716728a87d02df0cfd77b96111c32e0c2b
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-10-02 01:15:25 +0900

    [Gold V] Title: 전깃줄, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -2,60 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-        public static int N,M = 0;
-        static int[] dp;
-
+    public static int N,M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
-        ArrayList<Node> arr = new ArrayList<>();
-        dp= new int[N];
+        int[][] arr = new int[N][2];
+        int[] dp = new int[N];
 
-        for(int i =0;i<N;i++){
+        for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            arr.add(new Node(n,m));
+
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
+
 
         }
 
-        arr.sort((o1, o2) -> o1.x - o2.x);
 
-        dp[0] = 1;
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
 
-        for(int i=1;i<N;i++){
+
+        for(int i=0;i<N;i++) {
             dp[i] = 1;
-            for(int j=0;j<i;j++){
-                if(arr.get(i).y > arr.get(j).y){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+            for(int j=0;j<i;j++) {
+                if(arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
 
-        int max =Integer.MIN_VALUE;
-        for(int j=0;j<N;j++) {
-            if(dp[j]>max)
-                max = dp[j];
+        int max = 0;
+        for(int i=0; i<N;i++) {
+            max = Math.max(max, dp[i]);
         }
 
         System.out.println(N-max);
 
+
+
     }
 
-}
-
-class Node implements Comparable<Node> {
-    int x,y;
-    Node(int x, int y){
-        this.x=x;
-        this.y=y;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-
-        return  this.y - o.y;
-    }
 }
