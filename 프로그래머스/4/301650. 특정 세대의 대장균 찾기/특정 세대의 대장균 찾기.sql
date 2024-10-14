7ae7b414266a06c0b011280d4b636fe2e9315ae6
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-10-15 01:12:17 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,34 +1,57 @@
-WITH RECURSIVE Ancestors AS (
-    -- 1세대를 선택합니다 (최초의 조상)
-    SELECT 
-        ID, 
-        PARENT_ID, 
-        1 AS generation
-    FROM 
-        ECOLI_DATA
-    WHERE 
-        PARENT_ID IS NULL
-    
-    UNION ALL
-    
-    -- 재귀적으로 조상을 찾습니다
-    SELECT 
-        E.ID, 
-        E.PARENT_ID, 
-        A.generation + 1
-    FROM 
-        ECOLI_DATA E
-    JOIN 
-        Ancestors A ON E.PARENT_ID = A.ID
-    WHERE 
-        A.generation < 100  -- 100세대까지 제한
-)
+SELECT A.ID
+FROM ECOLI_DATA AS A
+JOIN ECOLI_DATA AS B ON A.PARENT_ID = B.ID
+JOIN ECOLI_DATA AS C ON B.PARENT_ID = C.ID
+WHERE C.PARENT_ID IS NULL
+ORDER BY A.ID ASC;
 
-SELECT 
-    ID
-FROM 
-    Ancestors
-WHERE 
-    generation = 3
-ORDER BY 
-    generation, ID;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# WITH RECURSIVE Ancestors AS (
+#     -- 1세대를 선택합니다 (최초의 조상)
+#     SELECT 
+#         ID, 
+#         PARENT_ID, 
+#         1 AS generation
+#     FROM 
+#         ECOLI_DATA
+#     WHERE 
+#         PARENT_ID IS NULL
+    
+#     UNION ALL
+    
+#     -- 재귀적으로 조상을 찾습니다
+#     SELECT 
+#         E.ID, 
+#         E.PARENT_ID, 
+#         A.generation + 1
+#     FROM 
+#         ECOLI_DATA E
+#     JOIN 
+#         Ancestors A ON E.PARENT_ID = A.ID
+#     WHERE 
+#         A.generation < 100  -- 100세대까지 제한
+# )
+
+# SELECT 
+#     ID
+# FROM 
+#     Ancestors
+# WHERE 
+#     generation = 3
+# ORDER BY 
+#     generation, ID;
