4ae837cf628f09b413a96208a876e47748bff494
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-10-15 19:28:17 +0900

    [level 2] Title: 오픈채팅방, Time: 169.86 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
+        String[] answer = {};
         Map<String,String> map = new HashMap<>();
         Queue<String[]> que = new LinkedList<>();
-        int n =0;
         for(String str : record){
             String[] action = str.split(" ");
             if(action[0].equals("Enter") || action[0].equals("Change")){
@@ -13,24 +13,22 @@ class Solution {
             }
             if(action[0].equals("Enter") || action[0].equals("Leave")){
                 que.add(new String[]{action[1],action[0]});
-                n++;
             }
             
         }
-        String[] answer = new String[n];
-        int count=0;
+        ArrayList<String> arr = new ArrayList<>();
         while(!que.isEmpty()){
             String[] act = que.poll();
             if(act[1].equals("Enter")){
-                answer[count++] = map.get(act[0]) + "님이 들어왔습니다.";
+                arr.add(map.get(act[0]) + "님이 들어왔습니다.");
                 
             }else if(act[1].equals("Leave")){
-                answer[count++] = map.get(act[0]) + "님이 나갔습니다.";
+                arr.add(map.get(act[0]) + "님이 나갔습니다.");
                 
             }
             
         }
-        
+        answer = arr.toArray(answer);
         return answer;
     }
 }
