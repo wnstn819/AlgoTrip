e7a2720db1fab3411d8314462730415b0c5646e9
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-10-16 18:22:54 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 9.96 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,17 +2,54 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int deliver = 0, pickup = 0;
-        for(int i = n-1; i >= 0; i--){
-            deliver += deliveries[i];
-            pickup += pickups[i];
-            while (deliver > 0 || pickup > 0){
-                deliver -= cap;
-                pickup -= cap;
-                answer += ((i+1) * 2);
+        int num = 0;
+        for(int i =n-1;i>=0;i--){
+            if(deliveries[i] > 0 || pickups[i] > 0){
+                num = i;
+                break;
             }
-
         }
+        
+        int d =0;
+        int p =0;
+        for(int i=num;i>=0;i--){
+            d += deliveries[i];
+            p += pickups[i];
+            
+            while(d > 0 || p > 0){
+                d -= cap;
+                p -= cap;
+                answer += ((i+1)*2);
+            }
+        
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         long answer = 0;
+//         int deliver = 0, pickup = 0;
+//         for(int i = n-1; i >= 0; i--){
+//             deliver += deliveries[i];
+//             pickup += pickups[i];
+//             while (deliver > 0 || pickup > 0){
+//                 deliver -= cap;
+//                 pickup -= cap;
+//                 answer += ((i+1) * 2);
+//             }
+
+//         }
         return answer;
     }
 }
