400ba92f62c2f4a32bbe773179008b215072b231
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-11-13 14:08:31 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static boolean[] visited;
+    public static int answer = 0;
+    
+    public int solution(String begin, String target, String[] words) {
+       
+        visited=new boolean[words.length];
+        
+        dfs(begin,target,words,0);
+        return answer;
+    }
+    
+    public static void dfs(String begin, String target, String[] words, int depth){
+        if(begin.equals(target)){
+            answer = depth;
+            return;
+        }
+        
+     
+        for(int i=0;i<words.length;i++){
+            if(visited[i]){
+                continue;
+            }
+            int count=0;
+            for(int j=0;j<begin.length();j++){
+                if(begin.charAt(j) == words[i].charAt(j)){
+                    count++;
+                }
+            }
+            
+            if(count == begin.length()-1){
+                visited[i] = true;
+                dfs(words[i],target,words,depth + 1);
+                visited[i] = false;
+            }
+            
+        }
+        
+    }
+}
