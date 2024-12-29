6fd9b651a480fb9b77e46113bc5c1484d5030fe6
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2024-12-29 21:54:15 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/README.md
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/README.md
@@ -1,6 +1,6 @@
 # [level 3] 있었는데요 없었습니다 - 59043 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59043) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59043?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 04월 26일 16:57:22
+2024년 12월 29일 21:54:12
 
 ### 문제 설명
 
--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,9 +1,10 @@
 -- 코드를 입력하세요
--- 관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다. 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
-SELECT ANIMAL_ID, I.NAME
--- SELECT *
-FROM ANIMAL_INS I
-JOIN ANIMAL_OUTS O
-USING(ANIMAL_ID)
-WHERE O.DATETIME <= I.DATETIME
-ORDER BY I.DATETIME
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_INS A
+JOIN ANIMAL_OUTS B
+ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE B.DATETIME < A.DATETIME
+ORDER BY A.DATETIME ASC;
+
+
+
